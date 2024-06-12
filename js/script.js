c0e9eb37be56a73edd9d2f921d3ff70da0de11a0
isnanw <isnan@gin.co.id>
2024-06-13 00:44:54 +0900

    About

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-$(document).ready(function () {
-  const text = "PORTFOLIO";
-  const typingText = $("#typing-text");
-  let index = 0;
-
-  function type() {
-    if (index < text.length) {
-      typingText.append(text.charAt(index));
-      index++;
-      setTimeout(type, 150); // Adjust the delay to control the typing speed
-    }
-  }
-
-  type();
-});
+// $(document).ready(function () {
+//   $(".menu ul li span, .menu ul li a span").on("click", function () {
+//     $(".menu ul li span, .menu ul li a span").removeClass("active");
+//     $(this).addClass("active");
+//   });
+// });
